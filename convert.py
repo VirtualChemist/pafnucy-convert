a2618f553a169678a585b91bfdc4944018658655
Joshua Spayd <joshua.spayd@icloud.com>
2019-11-08 22:35:10 -0800

    Initial script

---
+++ convert.py
@@ -0,0 +1,49 @@
+import csv
+import mechanize
+import os
+from pyquery import PyQuery
+import ssl
+import urllib.request
+
+HTML_SUCCESS = 200
+URL_TEMPLATE = "http://cactus.nci.nih.gov{}"
+FILE_TEMPLATE = "{}/{}.pdb"
+URL = URL_TEMPLATE.format('/translate')
+OUTDIR = 'pdb'
+CSV_FILE = 'smiles.csv'
+
+ssl._create_default_https_context = ssl._create_unverified_context
+br = mechanize.Browser()
+br.set_handle_robots(False)
+
+if not os.path.exists(OUTDIR):
+    os.makedirs(OUTDIR)
+
+with open(CSV_FILE, 'r', encoding='utf-8') as csvfile:
+    reader = csv.reader(csvfile)
+    rows = list(reader)
+    progress = 0
+    for common, smiles in rows:
+        progress += 1
+        print("Converting compound {}/{}: {}".format(progress, len(rows), common))
+        print('  Opening connection')
+        br.open(URL)
+        br.select_form(name='form')
+        br['smiles'] = smiles
+        br['format'] = ['pdb']
+        br['astyle'] = ['aromatic']
+        br['dim'] = ['3D']
+        print('  Submitting form')
+        res = br.submit()
+        if res.getcode() == HTML_SUCCESS:
+            print('  Downloading PDB file')
+            content = res.read()
+            pq = PyQuery(content)
+            link = pq('a:first').attr('href')
+            file_url = URL_TEMPLATE.format(link)
+            filename = FILE_TEMPLATE.format(OUTDIR, common)
+            urllib.request.urlretrieve(file_url, filename)
+            print("  Success! File saved to {}".format(filename))
+        else:
+            print("  Invalid form response: {}".format(res.getcode()))
+        print()
