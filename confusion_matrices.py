319f16c009913b715186c2e05dc2aa77ac2d2161
lemuelcardenas <lemuel@stanford.edu>
2019-12-01 13:18:33 -0800

    added confusion matrix script & baseline code

---
+++ confusion_matrices.py
@@ -0,0 +1,94 @@
+import os
+import math
+import baseline
+
+vinafolder = 'vina_logs/'
+blockedfolder = 'blocked/'
+metric = None
+
+false_pos = 0
+true_neg = 0
+false_neg = 0
+true_pos = 0
+
+
+while(True):
+	print('Which error metrics do you want?')
+	print('V for vina, P for original pafnucy, N for n-gram baseline')
+	command = input()
+	if len(command) > 1 or command not in 'VPN':
+		print()
+		print('Please input one of [V, P, N]')
+		print()
+		continue 
+	metric = command
+	break
+
+
+with open('affinities_binary.csv', 'r') as examples:
+	for j, example in enumerate(examples):
+		if j == 0: continue
+		row = example.split(',')
+		protein_ligand = row[0].replace('ligand_', '')
+		klass = int(row[1])
+
+		if metric == 'V':
+			with open(vinafolder + protein_ligand + '.txt', 'r') as f:
+				start = None
+				for i, line in enumerate(f):
+					vina_line = line.split()
+					if len(vina_line) == 0: continue
+					if vina_line[0] == 'mode':
+						start = i
+					if start != None:
+						if i - start == 3:
+							affinity = float(vina_line[1])
+							DeltaG = 1000 * affinity
+							Ki = math.exp(DeltaG/(1.986 * 298)) * (10 ** 9)
+							IC50 = Ki * 2
+							if klass == 0:
+								if IC50 >= 500:
+									#classification = 0 
+									true_neg += 1
+								else:
+									#classification = 1
+									false_pos += 1
+							else: 
+								if IC50 >= 500:
+									#classification = 0 
+									false_neg += 1
+								else:
+									#classification = 1
+									true_pos += 1
+							break
+		elif metric == 'P':
+			with open(blockedfolder +  protein_ligand + '.pdb/predictions.csv', 'r') as f:
+				for i, line in enumerate(f):
+					if i == 0: continue
+					pafnucy_line = line.split(',')
+					pKi = float(pafnucy_line[1])
+					IC50 = (10 ** (-pKi)) * (10 ** 9) * 2
+					if klass == 0:
+						if IC50 >= 500:
+							#classification = 0 
+							true_neg += 1
+						else:
+							#classification = 1
+							false_pos += 1
+					else: 
+						if IC50 >= 500:
+							#classification = 0 
+							false_neg += 1
+						else:
+							#classification = 1
+							true_pos += 1
+		else:
+			baseline.main()
+			exit()
+
+print('False Positive:', false_pos)
+print('True Negative:', true_neg)
+print('False Negative:', false_neg)
+print('True Positive:', true_pos)
+
+
