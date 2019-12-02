993192e228f64c036b067f77c5771964ec9e8168
lemuelcardenas <lemuel@stanford.edu>
2019-12-01 18:41:32 -0800

    bugfixing plot script

--- plot_og_paf_rmse.py
+++ plot_og_paf_rmse.py
@@ -20,19 +20,19 @@ count = 0
 with open('affinities.csv', 'r') as datapoints:
 	for j, datapoint in enumerate(datapoints):
 		if j == 0: continue
-			row = datapoint.split(',')
-			pdbid = row[0].replace('ligand_', '')
-			affin = float(row[1])
-			with open('blocked/' + pdbid + '.pdb/predictions.csv') as f:
-				for i, line in enumerate(f):
-					if i == 0: continue
-					pafnucy_line = line.split(',')
-					pred_val = float(pafnucy_line[1])
-				ids.append(pdbid)
-				affinity.append(affin)
-				pred.append(pred_val)
-				rmse += (pred_val - affin) ** 2
-				count += 1
+		row = datapoint.split(',')
+		pdbid = row[0].replace('ligand_', '')
+		affin = float(row[1])
+		with open('blocked/' + pdbid + '.pdb/predictions.csv') as f:
+			for i, line in enumerate(f):
+				if i == 0: continue
+				pafnucy_line = line.split(',')
+				pred_val = float(pafnucy_line[1])
+			ids.append(pdbid)
+			affinity.append(affin)
+			pred.append(pred_val)
+			rmse += (pred_val - affin) ** 2
+			count += 1
 	rmse = math.sqrt(rmse / count)
 
 
